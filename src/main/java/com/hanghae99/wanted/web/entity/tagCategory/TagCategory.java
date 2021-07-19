44f64f9e0d6f8978f18204be58beeb7276d5821c
tmddusgood <tmddusgood@gmail.com>
2021-07-19 11:40:50 +0900

    fix: Add missing fields, builder + Extend Tag class
    1. Tag Category is not to be deprecated
    2. Tag Category Repository is not to be deprecated
    3. TAG_ID -> TAG_CTGRY_ID on database
    4. User class extends BaseTimeEntity
    These changes applied on database as well.

--- src/main/java/com/hanghae99/wanted/web/entity/tagCategory/TagCategory.java
+++ src/main/java/com/hanghae99/wanted/web/entity/tagCategory/TagCategory.java
@@ -1,11 +1,9 @@
 package com.hanghae99.wanted.web.entity.tagCategory;
 
 import com.hanghae99.wanted.web.entity.BaseTimeEntity;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,21 +11,21 @@ import lombok.NoArgsConstructor;
 
 
 /**
- * 상위 카테고리 클래스는 사용하지 않을 것으로 예상됩니다. Created by Bloo, 21/07/17
- * @deprecated
+ * CloneProject.TAG_CATEGORY_TB와 매핑되는 Entity 클래스입니다. Created by Bloo, 21/07/16
  * @author Bloo
  * @author Seungyeon Kang
  */
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
+@Table(name = "TAG_CATEGORY_TB")
 public class TagCategory extends BaseTimeEntity {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "NAME")
     private String name;
 
     @Builder
